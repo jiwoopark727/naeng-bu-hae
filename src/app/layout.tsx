c832b989ea3304491a8f4ae739c2f868d246750e
Jiwoo Park <jiwoopark727@gmail.com>
2025-07-21 17:13:37 +0900

    feature: 메인 페이지 구현(헤더, 배경 및 냉장고 이미지 적용)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/globals.css';
+import Header from './components/Header';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className='bg-gray-400'>
+        <div className='min-h-[100dvh] w-full flex items-center justify-center bg-gray-300'>
+          <div className='w-full max-w-[393px] h-[100dvh] max-h-[852px] bg-[#fcfcfc] flex flex-col items-center'>
+            <Header />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import Image from 'next/image';
+import Background from '../../public/assets/images/Background.png';
+import Fridge from '../../public/assets/images/Fridge.png';
+import '@/app/globals.css';
+
 export default function Home() {
-  return <div>메인 페이지(냉장고 화면)</div>;
+  return (
+    <>
+      <div className='relative w-[393px] h-[774px]'>
+        {/* 배경이미지 */}
+        <Image
+          src={Background}
+          alt='Background'
+          width={393}
+          height={774}
+          className='absolute top-0 left-0 z-0'
+        />
+        {/* 겹쳐지는 냉장고 이미지 */}
+        <Image
+          src={Fridge}
+          alt='Fridge'
+          width={300}
+          height={200}
+          className='absolute top-[230px] left-[80px] z-10'
+        />
+        <div className='absolute top-[480px] left-[160px] z-20 font-semibold text-lg animate-pulse duration-300 cursor-pointer text-yellow-400'>
+          👈 Touch!!
+        </div>
+      </div>
+    </>
+  );
 }
