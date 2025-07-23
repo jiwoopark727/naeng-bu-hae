4601670ea100cefe7203585a2da7a8be979b4e7d
Jiwoo Park <jiwoopark727@gmail.com>
2025-07-23 14:01:25 +0900

    feature: 재료 리스트 모달(냉장고 문) 닫기 버튼 구현

--- src/app/components/IngredientList.tsx
+++ src/app/components/IngredientList.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function IngredientList() {
+  const router = useRouter();
+
+  const handleXclick = () => {
+    router.back();
+  };
+
   return (
     <div className='absolute top-50 right-5 z-20 text-md text-black w-70 h-130 bg-white rounded-[15px] opacity-95 p-4'>
       <div className='relative w-full h-full'>
-        <h1 className='text-lg font-semibold mb-4'>냉장고에 있는 재료</h1>
+        <span className='flex justify-between items-center mb-4'>
+          <p className='text-md font-semibold'>냉장고에 있는 재료</p>
+          <span className='px-2 cursor-pointer' onClick={handleXclick}>
+            X
+          </span>
+        </span>
 
         <div className='text-[13px]'>
           <ul>
