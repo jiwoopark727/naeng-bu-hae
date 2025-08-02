71adffaddcbac6842cbfc4eb873bd5278b8a4df0
asd45618 <asd45618@naver.com>
2025-08-03 00:39:58 +0900

    fix: useSearchParams 훅 사용으로 인한 빌드 오류 해결(Suspense로 감싸기)

--- src/app/(recipes)/recipes/page.tsx
+++ src/app/(recipes)/recipes/page.tsx
@@ -1,9 +1,12 @@
 import RecipeList from '@/app/components/RecipeList';
+import { Suspense } from 'react';
 
 export default function RecipesPage() {
   return (
     <div className='p-4 relative w-[393px] h-[756px] overflow-y-scroll [&::-webkit-scrollbar]:hidden'>
-      <RecipeList />
+      <Suspense>
+        <RecipeList />
+      </Suspense>
     </div>
   );
 }
